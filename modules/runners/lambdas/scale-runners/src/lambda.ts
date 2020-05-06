5facc717166accaa03eb0d0447e797d8820e0b54
Gertjan Maas <gertjan.maas@philips.com>
2020-05-06 13:17:47 +0200

    WIP: Scale lambda

--- modules/runners/lambdas/scale-runners/src/lambda.ts
+++ modules/runners/lambdas/scale-runners/src/lambda.ts
@@ -1,8 +1,11 @@
 import { handle } from './scale-runners/handler';
 
 module.exports.handler = async (event: any, context: any, callback: any) => {
-  const statusCode = await handle(event.headers, event.body);
-  return callback(null, {
-    statusCode: statusCode,
-  });
+  try {
+    await handle(event.eventSource, JSON.parse(event.body));
+    return callback(null);
+  } catch (e) {
+    console.error(e);
+    return callback('Failed handling SQS event');
+  }
 };
