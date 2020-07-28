ab2ba19e07a4906e9ece58fc0f41bcff1eb0a596
Alberto Alonso <alberto@alonso.xyz>
2020-07-28 22:03:47 +0300

    Fix the build script not entering all the module directories (#103)

--- .ci/build.sh
+++ .ci/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-lambdaSrcDirs=("modules/runner-binaries-syncer/lambdas/runner-binaries-syncer" "modules/runners/lambdas/scale-runners" "modules/webhook/lambdas/webhook")
-repoRoot=$(dirname "${BASH_SOURCE[0]}")/..
+lambdaSrcDirs=("modules/runner-binaries-syncer/lambdas/runner-binaries-syncer" "modules/runners/lambdas/runners" "modules/webhook/lambdas/webhook")
+repoRoot=$(dirname $(dirname $(realpath ${BASH_SOURCE[0]})))
 
 for lambdaDir in ${lambdaSrcDirs[@]}; do
-    cd $repoRoot/${lambdaDir}
+    cd "$repoRoot/${lambdaDir}"
     docker build -t lambda -f ../../../../.ci/Dockerfile .
     docker create --name lambda lambda
     zipName=$(basename "$PWD")
