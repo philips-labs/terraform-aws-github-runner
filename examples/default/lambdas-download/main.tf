07bed22569b3a8135bc4223705c6f344a8d19129
Niek Palm <dev.npalm@gmail.com>
2020-12-08 09:00:07 +0100

    Release v0.8.0

--- examples/default/lambdas-download/main.tf
+++ examples/default/lambdas-download/main.tf
@@ -3,15 +3,15 @@ module "lambdas" {
   lambdas = [
     {
       name = "webhook"
-      tag  = "v0.7.0"
+      tag  = "v0.8.0"
     },
     {
       name = "runners"
-      tag  = "v0.7.0"
+      tag  = "v0.8.0"
     },
     {
       name = "runner-binaries-syncer"
-      tag  = "v0.7.0"
+      tag  = "v0.8.0"
     }
   ]
 }
