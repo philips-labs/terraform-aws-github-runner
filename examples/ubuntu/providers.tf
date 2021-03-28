261c77d756bf4e98800ec517d21dae078d4546c4
Jesse Adelman <879972+boldandbusted@users.noreply.github.com>
2021-03-28 08:41:01 -0400

    chore(examples):  add version constraints to ubuntu example (#707)
    * Create version constraint on Random, and also move to 'required_providers' (post-Terraform 0.13 change).
    
    * Formatting.

--- examples/ubuntu/providers.tf
+++ examples/ubuntu/providers.tf
@@ -1,4 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      version = "3.20"
+    }
+    random = {
+      version = "3.1.0"
+    }
+  }
+}
+
+
 provider "aws" {
-  region  = local.aws_region
-  version = "3.20"
+  region = local.aws_region
+}
+
+provider "random" {
 }
