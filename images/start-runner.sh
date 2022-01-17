36c1bf5acda33f6e1498cf380a669df976fb12c6
Callum Tait <15716903+toast-gear@users.noreply.github.com>
2022-01-17 17:01:55 +0000

    fix(images): use new runner install location (#1628)

--- images/start-runner.sh
+++ images/start-runner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 exec > >(tee /var/log/runner-startup.log | logger -t user-data -s 2>/dev/console) 2>&1
 
-cd /home/ec2-user/actions-runner
+cd /opt/actions-runner
 
 ## This wrapper file re-uses scripts in the /modules/runners/templates directory
 ## of this repo. These are the same that are used by the user_data functionality 
