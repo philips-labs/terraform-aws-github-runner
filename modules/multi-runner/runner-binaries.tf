43acb08d94841ad5cdb3c3f08d078e67edcd84ea
Samuel Pelegrinello Caipers <71756456+spcaipers-arm@users.noreply.github.com>
2023-04-28 12:17:41 +0100

    fix(multi-runner):  convertdistribution_bucket_name to lowercase (#3219)
    refactor: force lowercase for distribution_bucket_name
    
    Co-authored-by: Niek Palm <npalm@users.noreply.github.com>

--- modules/multi-runner/runner-binaries.tf
+++ modules/multi-runner/runner-binaries.tf
@@ -4,7 +4,8 @@ module "runner_binaries" {
   prefix   = "${var.prefix}-${each.value.os_type}-${each.value.architecture}"
   tags     = local.tags
 
-  distribution_bucket_name = "${var.prefix}-${each.value.os_type}-${each.value.architecture}-dist-${random_string.random.result}"
+  # force mandatory lower case for s3 bucketname
+  distribution_bucket_name = lower("${var.prefix}-${each.value.os_type}-${each.value.architecture}-dist-${random_string.random.result}")
 
   runner_os           = each.value.os_type
   runner_architecture = each.value.architecture
