41a74ec6203e8a5f6af96fa7c054724108b08874
Navdeep Gupta <navdeep.gupta@philips.com>
2023-07-05 14:08:34 -0400

    fix: add more outputs to multi runners module. (#3343)
    * fix: chnages multi runners output from array to a map.
    
    * fix: updated binaries_syncer output as well.
    
    * fix: updated to add extra variables.
    
    * fix: added deprication for previous outputs.
    
    * fix: description.

--- modules/multi-runner/outputs.tf
+++ modules/multi-runner/outputs.tf
@@ -1,4 +1,5 @@
 output "runners" {
+  description = "(Deprecated, no longer used), see runners_map."
   value = [for runner in module.runners : {
     launch_template_name    = runner.launch_template.name
     launch_template_id      = runner.launch_template.id
@@ -18,8 +19,30 @@ output "runners" {
     logfiles                = runner.logfiles
   }]
 }
+output "runners_map" {
+  value = { for runner_key, runner in module.runners : runner_key => {
+    launch_template_name    = runner.launch_template.name
+    launch_template_id      = runner.launch_template.id
+    launch_template_version = runner.launch_template.latest_version
+    launch_template_ami_id  = runner.launch_template.image_id
+    lambda_up               = runner.lambda_scale_up
+    lambda_up_log_group     = runner.lambda_scale_up_log_group
+    lambda_down             = runner.lambda_scale_down
+    lambda_down_log_group   = runner.lambda_scale_down_log_group
+    lambda_pool             = runner.lambda_pool
+    lambda_pool_log_group   = runner.lambda_pool_log_group
+    role_runner             = runner.role_runner
+    role_scale_up           = runner.role_scale_up
+    role_scale_down         = runner.role_scale_down
+    role_pool               = runner.role_pool
+    runners_log_groups      = runner.runners_log_groups
+    logfiles                = runner.logfiles
+    }
+  }
+}
 
 output "binaries_syncer" {
+  description = "(Deprecated, no longer used), see binaries_syncer_map."
   value = [for runner_binary in module.runner_binaries : {
     lambda           = runner_binary.lambda
     lambda_log_group = runner_binary.lambda_log_group
@@ -27,6 +50,16 @@ output "binaries_syncer" {
     location         = "s3://runner_binary.bucket.id}/runner_binary.bucket.key"
     bucket           = runner_binary.bucket
   }]
+}
+
+output "binaries_syncer_map" {
+  value = { for runner_binary_key, runner_binary in module.runner_binaries : runner_binary_key => {
+    lambda           = runner_binary.lambda
+    lambda_log_group = runner_binary.lambda_log_group
+    lambda_role      = runner_binary.lambda_role
+    location         = "s3://runner_binary.bucket.id}/runner_binary.bucket.key"
+    bucket           = runner_binary.bucket
+  } }
 }
 
 output "webhook" {
