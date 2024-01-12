93537347538a8a57516ae1737b96d4c37319af32
Niek Palm <npalm@users.noreply.github.com>
2024-01-12 09:38:36 +0100

    fix(multi-runner): remove renundant default labels (#3715)
    The multi runner module is adding on two places the default labels like
    `self-hosted`, `os` and `arch`. The PR ensures the default labels are
    only added in
    [`multi-runner/runners.tf`](https://github.com/philips-labs/terraform-aws-github-runner/blob/4f257f9ff643e1f31e597a15dd9a0a65afbdc605/modules/multi-runner/runners.tf#L48)

--- modules/multi-runner/main.tf
+++ modules/multi-runner/main.tf
@@ -8,7 +8,7 @@ locals {
     key_base64 = module.ssm.parameters.github_app_key_base64
   }
 
-  runner_extra_labels = { for k, v in var.multi_runner_config : k => sort(setsubtract(setunion(flatten(v.matcherConfig.labelMatchers), compact(v.runner_config.runner_extra_labels)), ["self-hosted", v.runner_config.runner_os, v.runner_config.runner_architecture])) }
+  runner_extra_labels = { for k, v in var.multi_runner_config : k => sort(setunion(flatten(v.matcherConfig.labelMatchers), compact(v.runner_config.runner_extra_labels))) }
 
   runner_config = { for k, v in var.multi_runner_config : k => merge({ id = aws_sqs_queue.queued_builds[k].id, arn = aws_sqs_queue.queued_builds[k].arn }, merge(v, { runner_config = merge(v.runner_config, { runner_extra_labels = local.runner_extra_labels[k] }) })) }
 
