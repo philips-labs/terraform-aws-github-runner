303815c5d862da330696f73dad745809874aec70
Niek Palm <niek.palm@philips.com>
2024-11-16 16:55:18 +0100

    ci: refactor terraform validation

--- .utils/terraform.sh
+++ .utils/terraform.sh
@@ -10,3 +10,32 @@ generateDummyLambdaZip() {
     touch "$lambdaDir/$(basename $lambdaDir).zip"
   done
 }
+
+tfLintModules() {
+  examples=($( findAllTerraformDirs --ignores modules --hide-root --format plain))
+  ignore_commands=""
+  # example
+  # tflint --ignore-module terraform-aws-modules/vpc/aws --ignore-module terraform-aws-modules/security-group/aws
+  # otuput of find
+#   ./examples/arm64
+# ./examples/arm64/lambdas-download
+# ./examples/base
+# ./examples/default
+# ./examples/ephemeral
+# ./examples/lambdas-download
+# ./examples/multi-runner
+# ./examples/permissions-boundary
+# ./examples/permissions-boundary/setup
+# ./examples/prebuilt
+# ./examples/termination-watcher
+# ./examples/ubuntu
+# ./examples/windows
+# ./examples/windows/lambdas-download
+  echo hi
+  # for the ignore we need to strip ./ at the beginning
+  for example in "${examples[@]}"; do
+    ignore_commands="$ignore_commands --ignore-module=$example"
+  done
+  # run tflint
+  echo tflint --recursive $ignore_commands --config "$(pwd)/.tflint.hcl"
+}
