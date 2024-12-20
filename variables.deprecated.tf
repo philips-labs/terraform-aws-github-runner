099c78dc5db0ccf71277b1d4941e05e72336cff8
Niek Palm <npalm@users.noreply.github.com>
2024-12-20 14:17:46 +0100

    fix!: remove deprecated variables (#4073)
    Removing deprecated variables
    
    - `lambda_tracing_mode`
    - `enable_event_rule_binaries_syncer`
    - `runners_scale_up_Lambda_memory_size`
    
    ---------
    
    Co-authored-by: forest-pr|bot <forest-pr[bot]@users.noreply.github.com>
    Co-authored-by: philips-labs-pr|bot <philips-labs-pr[bot]@users.noreply.github.com>

--- variables.deprecated.tf
+++ variables.deprecated.tf
@@ -1,47 +1,14 @@
+# Move deprecated variables to this file
+
+# EXAMPLE
 # tflint-ignore: terraform_unused_declarations
-variable "lambda_tracing_mode" {
-  description = "DEPRECATED: Replaced by `tracing_config`."
-  type        = string
-  default     = null
+# variable "name" {
+#   description = "DEPRECATED: decription`."
+#   type        = string
+#   default     = null
 
-  validation {
-    condition     = anytrue([var.lambda_tracing_mode == null])
-    error_message = "DEPRECATED, Replaced by `tracing_config`."
-  }
-}
-
-# tflint-ignore: terraform_unused_declarations
-variable "enable_event_rule_binaries_syncer" {
-  description = "DEPRECATED: Replaced by `state_event_rule_binaries_syncer`."
-  type        = bool
-  default     = null
-  validation {
-    condition     = var.enable_event_rule_binaries_syncer == null
-    error_message = "DEPRECATED, Replaced by `state_event_rule_binaries_syncer`."
-  }
-}
-
-# tflint-ignore: terraform_unused_declarations
-variable "enable_metrics_control_plane" {
-  description = "(Experimental) Enable or disable the metrics for the module. Feature can change or renamed without a major release."
-  type        = bool
-  default     = null
-
-  # depcreated
-  validation {
-    condition     = var.enable_metrics_control_plane == null
-    error_message = "The variable `enable_metrics_control_plane` is deprecated, use `metrics.enabled` instead."
-  }
-}
-
-# tflint-ignore: terraform_unused_declarations
-variable "metrics_namespace" {
-  description = "The namespace for the metrics created by the module. Merics will only be created if explicit enabled."
-  type        = string
-  default     = null
-
-  validation {
-    condition     = var.metrics_namespace == null
-    error_message = "The variable `metrics_namespace` is deprecated, use `metrics.namespace` instead."
-  }
-}
+#   validation {
+#     condition     = ...
+#     error_message = "DEPRECATED, Replaced by `...`."
+#   }
+# }
